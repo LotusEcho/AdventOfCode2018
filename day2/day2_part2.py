ecd5989d760d575c18a32662010169b5bb50d2ff
LotusEcho <342531+LotusEcho@users.noreply.github.com>
2018-12-02 17:26:18 -0600

    Added handling of two more edge cases to day2_part2. Added documentation and doctests to some methods.

--- day2/day2_part2.py
+++ day2/day2_part2.py
@@ -1,32 +1,45 @@
 import os
 from helper.input_output import file
 
+
 def compare_strings_in_list(list_of_strings):
+    """Compares the list of strings recursively, failing fast when it finds more than 1 non-matching character.
+
+    :param list_of_strings: The list of strings to iterate through, which gets smaller by one with each loop.
+    :return: The two strings that match.
+
+    ---------------------------------------------------------------------------------------------
+
+    >>> compare_strings_in_list(["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"])
+    ('fghij', 'fguij')
+    >>> compare_strings_in_list(["", "", "", "", "", ""])
+    >>> compare_strings_in_list(["abcsdad", "abbsfdsf", "sdafjga"])
+    Traceback (most recent call last):
+    ...
+    ValueError: The two strings had different lengths
+    >>> compare_strings_in_list(["teststring", "teststring"])
+    ('teststring', 'teststring')
+    >>>
+    """
     if len(list_of_strings) > 1:
         string_to_compare = list_of_strings.pop(0)
-        for other_string in list_of_strings:
-            diff_count = 0
-            if len(string_to_compare) != len(other_string):
-                raise ValueError("The two strings had different lengths")
-            for index in range(0, len(string_to_compare)):
-                if string_to_compare[index] != other_string[index]:
-                    diff_count += 1
-                if diff_count > 1:
-                    break
-            if diff_count == 1:
-                return string_to_compare, other_string
-        return compare_strings_in_list(list_of_strings)
-    else:
-        return "", ""
+        if len(string_to_compare) > 0:
+            for other_string in list_of_strings:
+                if len(other_string) > 0:
+                    diff_count = 0
+                    if len(string_to_compare) != len(other_string):
+                        raise ValueError("The two strings had different lengths")
+                    for index in range(0, len(string_to_compare)):
+                        if string_to_compare[index] != other_string[index]:
+                            diff_count += 1
+                        if diff_count > 1:
+                            break
+                    if diff_count <= 1:
+                        return string_to_compare, other_string
+            return compare_strings_in_list(list_of_strings)
 
 
 filename = os.path.join(os.path.dirname(__file__), 'day2_input.txt')
-test_list = ["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"]
-first_test_string, second_test_string = compare_strings_in_list(test_list)
-assert(first_test_string == "fghij")
-assert(second_test_string == "fguij")
-print(f"TEST: Found matching strings '{first_test_string}' and '{second_test_string}'")
-
 line_list, matches = file.load_file_list_function(filename, str.rstrip, compare_strings_in_list)
 print(f"Real: Found matching strings '{matches[0]}' and '{matches[1]}'.")
 answer = ""
--- helper/input_output/file.py
+++ helper/input_output/file.py
@@ -1,3 +1,10 @@
+import os
+
+
+def __test_line_func(entry):
+    new_string = str.rstrip(entry)
+    return new_string, new_string.upper()
+
 
 def load_file_iterative_function(file_name, some_func, **kwargs):
     """Loads a file from an absolute path and then does something to its input with each iteration.
@@ -7,6 +14,14 @@ def load_file_iterative_function(file_name, some_func, **kwargs):
     This function should return the formatted line item and result of its calculation.
     :param kwargs: The arguments that you might need to pass to the function, as keyword arguments.
     :return: The file's contents as a list and the results of the function as a list
+
+    ---------------------------------------------------------------------------------------------
+
+    >>> load_file_iterative_function(os.path.join(os.path.dirname(__file__), 'testfile.txt'), __test_line_func)
+    Lines in file: 4
+    (['This is a story', 'all about how', 'my code got flip', 'turned upside down'], \
+['THIS IS A STORY', 'ALL ABOUT HOW', 'MY CODE GOT FLIP', 'TURNED UPSIDE DOWN'])
+    >>>
     """
     file = open(file_name, 'r')
     line_list = list()
@@ -16,6 +31,8 @@ def load_file_iterative_function(file_name, some_func, **kwargs):
         line_list.append(formatted_line)
         result_list.append(output_entry)
     file.close()
+    print(f"Lines in file: {len(line_list)}")
+
     return line_list, result_list
 
 
@@ -27,6 +44,12 @@ def load_file_list_function(file_name, format_func, some_func, **kwargs):
     :param format_func: The function to use on the input to format it in the way the function wants.
     :param kwargs: The arguments that you might need to pass to the function, as keyword arguments.
     :return: The file's contents as a list and the results of the function.
+
+    ---------------------------------------------------------------------------------------------
+
+    >>> load_file_list_function(os.path.join(os.path.dirname(__file__), 'testfile.txt'), str.rstrip, len)
+    Lines in file: 4
+    (['This is a story', 'all about how', 'my code got flip', 'turned upside down'], 4)
     """
 
     file = open(file_name, 'r')
