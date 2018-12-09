3cf0487bde0bfc1b97d555fc6532302503cf1eba
LotusEcho <342531+LotusEcho@users.noreply.github.com>
2018-12-09 16:47:02 -0600

    Part 2 answer. Removed a loop. Removed variable rotation. Removed the index.

--- src/day9/day9.py
+++ src/day9/day9.py
@@ -28,42 +28,27 @@ def find_high_score(player_num, last_marble_value):
     marble_locations = deque()
     marble_locations.append(0)
     current_marble_value = 1
-    current_index = 0
+    player_id = 1
     while current_marble_value <= last_marble_value:
-        for player_id in range(1, player_num + 1):
-            if current_marble_value % 23 == 0:
-                player_scores[player_id] += current_marble_value
-                current_index -= 7
-                if current_index < 0:
-                    current_index = len(marble_locations) + current_index
-                marble_to_remove = marble_locations[current_index]
-                player_scores[player_id] += marble_to_remove
-                marble_locations.rotate(-current_index)
-                marble_locations.popleft()
-                marble_locations.rotate(current_index)
-            elif current_marble_value == 1 or len(marble_locations) == current_index + 2:
-                marble_locations.append(current_marble_value)
-                current_index = len(marble_locations)
-            elif current_index + 2 <= len(marble_locations):
-                current_index += 2
-                current_len = len(marble_locations)
-                marble_locations.insert(current_index, current_marble_value)
-                assert current_len+1 == len(marble_locations)
-            else:
-                current_index = 1
-                current_len = len(marble_locations)
-                marble_locations.insert(1, current_marble_value)
-                assert current_len+1 == len(marble_locations)
-            current_marble_value += 1
-            if current_marble_value > last_marble_value:
-                break
+        if current_marble_value % 23 == 0:
+            player_scores[player_id] += current_marble_value
+
+            marble_locations.rotate(7)
+            marble_to_remove = marble_locations.pop()
+            player_scores[player_id] += marble_to_remove
+            marble_locations.rotate(-1)
+        else:
+            marble_locations.rotate(-1)
+            marble_locations.append(current_marble_value)
+        current_marble_value += 1
+        player_id += 1
+        if player_id > player_num:
+            player_id = 1
 
     high_score = 0
-    winning_player = -1
     for player in player_scores.keys():
         if player_scores[player] > high_score:
             high_score = player_scores[player]
-            winning_player = player
     return high_score
 
 
